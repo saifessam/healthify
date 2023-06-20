1f2460eee5ff2448f4fbc64736a7a9e314ad4f8e
Saif Essam <saifeldeenessam01@gmail.com>
2023-06-20 16:11:11 +0300

    Removed `useTypedSelector`

--- hooks/typed-selector.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-import { RootState } from '../types/redux';
-
-const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export default useTypedSelector;
